8e7f2b61f7904252b5d64e0cd82d70fa66dc55b1
Nazeer Shaikh <nazeer.shaikh54@gmail.com>
2026-07-16 22:54:29 -0400

    Harden iOS browser navigation boundary

--- ios/PhoneDexTests/PhoneDexBrowserTests.swift
+++ ios/PhoneDexTests/PhoneDexBrowserTests.swift
@@ -15,4 +15,28 @@ final class PhoneDexBrowserTests: XCTestCase {
 
         XCTAssertNil(model.shareURL)
     }
+
+    func testShareURLFailsClosedForUnsupportedScheme() {
+        let model = PhoneDexBrowserModel()
+        model.address = "javascript:alert(1)"
+
+        XCTAssertNil(model.shareURL)
+    }
+
+    func testShareURLFailsClosedForEmbeddedCredentials() {
+        let model = PhoneDexBrowserModel()
+        model.address = "https://user:password@example.com"
+
+        XCTAssertNil(model.shareURL)
+    }
+
+    func testShareURLAllowsHTTPAndHTTPSHosts() {
+        let model = PhoneDexBrowserModel()
+
+        model.address = "http://example.com/path"
+        XCTAssertEqual(model.shareURL?.absoluteString, "http://example.com/path")
+
+        model.address = "https://example.com/path"
+        XCTAssertEqual(model.shareURL?.absoluteString, "https://example.com/path")
+    }
 }
